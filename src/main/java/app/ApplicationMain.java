f006c601b481e9c5be197fea8d241452d5287244
gbshiva <gbshiva@yahoo.com>
2018-01-08 12:16:51 -0500

    BackBase - Predective analytics

---
+++ src/main/java/app/ApplicationMain.java
@@ -0,0 +1,70 @@
+package app;
+
+import app.utils.SparkUtils;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+
+import lombok.Data;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import org.apache.log4j.Logger;
+
+import static spark.Spark.get;
+
+public class ApplicationMain {
+	private static customerModel model = new customerModel();
+
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(ApplicationMain.class);
+        SparkUtils.createServerWithRequestLog(logger);
+
+        get("/predict", (request, response) -> {
+        	response.status(200);
+            response.type("application/json");
+            return dataToJson(model.predict(request.queryParams("id")));
+        });
+        		
+        		
+        		
+        		
+        		
+    }
+
+    public static String dataToJson(Object data) {
+        try {
+        	
+        	//List dt = (List) data;
+        	//System.out.println(((cust)dt.get(1)).getName());
+        	
+        	
+           ObjectMapper mapper = new ObjectMapper();
+        	
+        	//ObjectMapper mapper = new XmlMapper();
+        	
+        	
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            StringWriter sw = new StringWriter();
+            mapper.writeValue(sw, data);
+            return sw.toString();
+        } catch (IOException e){
+        	e.printStackTrace(System.out);
+        	throw new RuntimeException("IOException from a StringWriter?");
+            
+        }
+    }
+}
---
+++ src/main/java/app/customerModel.java
@@ -0,0 +1,16 @@
+package app;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
+public class customerModel {
+	
+	public List predict(String customerID) throws Exception{
+		return null;
+	}
+
+}
